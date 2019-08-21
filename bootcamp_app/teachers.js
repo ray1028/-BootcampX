3a9807f1d7eb6429b19ba9ae116b50c9ada72143
rayjay1028 <rayjay1028@gmail.com>
2019-08-21 18:54:54 +0000

    completed tasks for adding parameterized query for sql query

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,18 @@
 const { Pool } = require("pg");
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
+
+const queryStr =     `
+select c.name, t.name as teacher
+from cohorts c 
+join students s
+on c.id = s.cohort_id
+join assistance_requests ar
+on s.id = ar.student_id
+join teachers t
+on ar.teacher_id = t.id
+where c.name like $1
+`
 
 const pool = new Pool({
   user: "vagrant",
@@ -8,19 +22,7 @@ const pool = new Pool({
 });
 
 pool
-  .query(
-    `
- select c.name, t.name as teacher
- from cohorts c 
- join students s
- on c.id = s.cohort_id
- join assistance_requests ar
- on s.id = ar.student_id
- join teachers t
- on ar.teacher_id = t.id
- where c.name like '%${process.argv[2]}%'
-`
-  )
+  .query(queryStr, values)
   .then(res => {
     res.rows.forEach(ele => console.log(`${ele.name} : ${ele.teacher}`));
   })
